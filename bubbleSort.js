44f15f323ffa1ee44e42fb18fbd0ec3ec341d6dd
Mostafa Khalid <mostafakhalid0996@gmail.com>
2019-11-01 17:03:09 +0200

    implement bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,23 @@
+/**
+ * Bubble sort is a comparison-based algorithm in which each pair of adjacent elements is compared and the elements
+ * are swapped if they are not in order. This algorithm is not suitable for large data sets as its average and worst case
+ * complexity are of Ο(n^2) where n is the number of items.
+ */
+ 
+ const bubbleSort = (arr) => {
+     for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length ; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] =  arr[j+1];
+                arr[j+1] = temp;
+                
+                // Es6 swap
+               // [ arr[j], arr[j+1]] = [arr[j+1],  arr[j]];
+            }
+        }
+    }
+     return arr
+ }         
+ // examples 
+console.log(bubbleSort([10,80,100,9,13])); // [9, 10, 13, 80, 100 ]
